d35704e0447c04c92aeb0799f9b6c1b11bb82b5e
rajdev1009 <mrdev4772@gmail.com>
2026-02-09 17:31:38 +0530

    Update service worker to version 2

--- sw.js
+++ sw.js
@@ -1,26 +1,4 @@
-const CACHE_NAME = 'astratoonix-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './logo.png'
-];
-
-// Install Service Worker
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
-});
-
-// Fetch Assets
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
-        })
-    );
-});
+const CACHE_NAME = 'astratoonix-v2';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './logo.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
