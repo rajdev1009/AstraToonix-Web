c1709ee51b38e2a187596aaec02e788707f22ca8
rajdev1009 <mrdev4772@gmail.com>
2026-02-11 18:10:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
-const CACHE_NAME = 'astratoonix-v2';
-const ASSETS = ['./', './index.html', './style.css', './script.js', './logo.png'];
+const CACHE_NAME = 'astratoonix-v3'; // Version change kar diya
+const ASSETS = [
+    './', 
+    './index.html', 
+    './shop.html',       // Shop page add kiya
+    './shop-data.js',    // New data file add ki
+    './style.css', 
+    './script.js', 
+    './logo.png'
+];
+
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
